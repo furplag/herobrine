627f1e27f9f0a7daba87b8f6c91c0d4d9f3e6d31
David Berdik <berdikgit@theprogrammersworld.net>
2022-02-23 22:08:24 -0500

    Add Kyori adventure API

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	`java-library`
 	id("io.papermc.paperweight.userdev") version "1.3.3"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
@@ -24,10 +25,15 @@ dependencies {
     implementation(":Towny")
     implementation(":WorldEdit")
     implementation(":WorldGuard")
+    shadow("net.kyori:adventure-api:4.9.3")
     paperDevBundle("1.18.1-R0.1-SNAPSHOT")
 }
 
 tasks {
+	shadowJar {
+        configurations = listOf(project.configurations.shadow.get())
+    }
+
 	assemble {
 		dependsOn(reobfJar)
 	}
