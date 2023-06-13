237acdd6814631f16b5403b436b8ac4b79083fa0
furplag <furplag@furplag.jp>
2023-06-13 11:01:45 +0900

    Update for Spigot-1.20

--- src/main/java/net/theprogrammersworld/herobrine/Herobrine.java
+++ src/main/java/net/theprogrammersworld/herobrine/Herobrine.java
@@ -83,7 +83,7 @@ public class Herobrine extends JavaPlugin implements Listener {
 		// If it is not, print an error message and disable the plugin.
 		if (continueWithEnable) {
 			try {
-				Class.forName("org.bukkit.craftbukkit.v1_19_R3.CraftArt");
+				Class.forName("org.bukkit.craftbukkit.v1_20_R1.CraftArt");
 			} catch (ClassNotFoundException e) {
 				Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "This version of Herobrine is not "
 						+ "compatible with this server's Spigot version and will be disabled.");
